d0d8c52e3d81eabbb0853ced7aae810b8fdbd6e6
rgayon-fr <rgayon@rgsoft.fr>
2022-02-26 09:34:34 +0100

    Added response to /metrics !

--- main.go
+++ main.go
@@ -19,7 +19,53 @@ var (
 )
 
 func handleMetricsRequest(w http.ResponseWriter, r *http.Request) {
-	// Get data from chage
+	// Init array of prometheus metric strings
+	var prometheusMetrics []PrometheusMetric
+	// Get data from chage for every user in relevant users.
+	for _, user := range relevantUsers {
+		chageData, chageError := GetChage(user)
+		// An error here shouldn't crash the software, notify user before proceeding with the next loop.
+		if chageError != nil {
+			log.Printf("error getting chage for %s: %s", user, chageError)
+			continue
+		}
+
+		// Return how much time before password change.
+		var daysBeforePwExpire int
+		timeBeforePwExpire := chageData.UntilExpiryDate()
+
+		// If the password doesn't expire, just return a big negavive number.
+		// If it does, just return the actual value.
+		if timeBeforePwExpire == 0 {
+			daysBeforePwExpire = -999
+		} else {
+			daysBeforePwExpire = int(chageData.UntilExpiryDate().Hours() / 24)
+		}
+		prometheusMetrics = append(prometheusMetrics, PrometheusMetric{
+			Name:  "chage_user_pw_expire_days",
+			Flags: []PrometheusFlag{{Key: "user", Value: user}},
+			Value: daysBeforePwExpire,
+		})
+
+		// Return the password age
+		prometheusMetrics = append(prometheusMetrics, PrometheusMetric{
+			Name:  "chage_user_pw_age_days",
+			Flags: []PrometheusFlag{{Key: "user", Value: user}},
+			Value: int(chageData.PasswordAge().Hours() / 24),
+		})
+	}
+
+	// For each prometheus metric, inject response with its rendered format.
+	for _, metric := range prometheusMetrics {
+		renderedMetric, renderError := metric.RenderMetric()
+		// Again, an error here shoudn't really hinder the result of the whole thing... warn user and move on.
+		if renderError != nil {
+			log.Printf("error rendering prometheus metric: %s", renderError)
+			continue
+		}
+		fmt.Fprintln(w, renderedMetric)
+	}
+
 	// Use template to send data back to Prometheus
 }
 
@@ -31,11 +77,13 @@ func main() {
 
 	// Init configuration structure
 	var config Config
+
 	// Read file
 	configFileData, configFileError := ioutil.ReadFile(*configPath)
 	if configFileError != nil {
 		log.Fatalf("error reading the config file: %s", configFileError)
 	}
+
 	// Unmarshall YAML data
 	parseError := yaml.Unmarshal(configFileData, &config)
 	if parseError != nil {
